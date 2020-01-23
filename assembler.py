dd5882134c97c3bdf0bd4efb87a309e5b678f6d6
BrooksPoltl <bpoltl1@gmail.com>
2020-01-23 05:59:55 -0600

    created hack string to write to

--- assembler.py
+++ assembler.py
@@ -1,7 +1,16 @@
 def main(file):
     f = open(file)
-    print(f)
-    for line in f:
-        print(line)
+    hack_file = file
+    index = len(hack_file) - 1  
+    while hack_file[index] in reversed(hack_file):
+        if(hack_file[index] == "."):
+            break
+        hack_file = hack_file[:-1]
+        index -= 1
 
+    hack_file = "".join((hack_file, "hack"))
+    
+    print(hack_file)
+    for line in f:
+        break
 main("./test/add/Add.asm")
