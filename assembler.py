989c9298cec9e9b7f035d39a86056128e04dba79
BrooksPoltl <bpoltl1@gmail.com>
2020-01-24 07:24:50 -0600

    can handle register access. forgot to read chapter 5

--- assembler.py
+++ assembler.py
@@ -1,19 +1,30 @@
 from numpy import binary_repr
+
+def line_parser(hack_file, line):
+    if(line[0] == "@"):
+        print("reached")
+        new_line = line[1:]
+        new_line = int(new_line)
+        hack_file.write(binary_repr(new_line, 16)+ '\n')
+    if(line == "D=A"):
+        hack_file.write("")
+
+
 def main(file):
     f = open(file)
     hack_file = file
     index = len(hack_file) - 1  
+    #create hackfile string
     while hack_file[index] in reversed(hack_file):
         if(hack_file[index] == "."):
             break
         hack_file = hack_file[:-1]
         index -= 1
     hack_file = "".join((hack_file, "hack"))
-    h = open(hack_file, "w+")
-    h.write(binary_repr(2, 16))
 
-    print(hack_file)
+
+    h = open(hack_file, "w+")
 
     for line in f:
-        break
+        line_parser(h, line)
 main("./test/add/Add.asm")
